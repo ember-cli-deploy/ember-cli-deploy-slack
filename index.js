2e2f6c20bcb8e7b56a12870933bd2263be40a94e
Michael Klein <michael@firstiwaslike.com>
2015-06-04 17:38:39 +0200

    First working version

--- index.js
+++ index.js
@@ -1,6 +1,75 @@
 /* jshint node: true */
 'use strict';
 
+var Promise       = require('ember-cli/lib/Ext/promise');
+var SilentError   = require('ember-cli/lib/errors/silent');
+var SlackNotifier = require('./lib/slack-notifier');
+var defaults      = require('./lib/default-config');
+var chalk         = require('chalk');
+
+var blue   = chalk.blue;
+var yellow = chalk.yellow;
+
+function applyDefaultConfigIfNecessary(config, prop, defaultConfig, ui){
+  if (!config[prop]) {
+    var value = defaultConfig[prop];
+    config[prop] = value;
+    ui.write(blue('|    '));
+    ui.writeLine(yellow('- Missing config: `' + prop + '`, using default: `' + value + '`'));
+  }
+}
+
+function initSlackNotifier(config, context) {
+  var webhookURL = config.webhookURL;
+
+  if (!webhookURL) {
+    var message   = 'Ember-CLI-Deploy: You have to pass a `webhookURL` config-option to ember-cli-deploy-slack';
+    context.slack = context.slack || {};
+
+    context.slack.error = message;
+
+    throw new SilentError(message);
+  }
+
+  var channel   = config.channel;
+  var username  = config.username;
+
+  return new SlackNotifier({
+    webhookURL: webhookURL,
+    channel: channel,
+    username: username
+  });
+}
+
+function executeSlackNotificationHook(context, hookName) {
+    var deployment = context.deployment;
+    var ui         = deployment.ui;
+    var config     = deployment.config[this.name] = deployment.config[this.name] || {};
+    var slack      = initSlackNotifier(config, context);
+
+    applyDefaultConfigIfNecessary(config, hookName, defaults, ui);
+
+    return config[hookName](context, slack);
+}
+
 module.exports = {
-  name: 'ember-cli-deploy-slack'
+  name: 'ember-cli-deploy-slack',
+
+  createDeployPlugin: function(options) {
+    return {
+      name: options.name,
+
+      willDeploy: function(context) {
+        return executeSlackNotificationHook.bind(this)(context, 'willDeploy');
+      },
+
+      didDeploy: function(context) {
+        return executeSlackNotificationHook.bind(this)(context, 'didDeploy');
+      },
+
+      didFail: function(context) {
+        return executeSlackNotificationHook.bind(this)(context, 'didFail');
+      }
+    }
+  }
 };
---
+++ node-tests/unit/slack-test.js
@@ -0,0 +1,65 @@
+var expect        = require('chai').expect;
+var sinon         = require('sinon');
+var Slack = require('node-slackr');
+var SlackNotifier = require('../../lib/slack-notifier.js');
+var Promise    = require('ember-cli/lib/Ext/promise');
+
+var WEBHOOK_URL = 'https://hooks.slack.com/services/123123';
+var CHANNEL     = '#testing';
+var USER_NAME   = 'ember-cli-deploy';
+
+var slackNotify;
+
+describe('SlackNotifier', function() {
+  var slack;
+  var sandbox;
+
+  beforeEach(function() {
+    slack = new SlackNotifier({
+      webhookURL: WEBHOOK_URL,
+      channel: CHANNEL,
+      username: USER_NAME
+    });
+
+    sandbox = sinon.sandbox.create();
+    slackNotify = sandbox.spy(Slack.prototype, 'notify');
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  it('it can be initialized', function() {
+    expect(slack).to.be.ok;
+    expect(slack.webhookURL).to.eql(WEBHOOK_URL);
+    expect(slack.channel).to.eql(CHANNEL);
+    expect(slack.username).to.eql(USER_NAME);
+  });
+
+  describe('#notify', function() {
+    it('is callable', function() {
+      expect(typeof(slack.notify)).to.eql('function');
+    });
+
+    it('sends the correct params to the node-slackr library', function() {
+      var messages = {
+          text: "using node-slackr to send messages to slack"
+      };
+
+      slack.notify(messages);
+
+      expect(slackNotify.calledWith(messages)).to.be.ok;
+    });
+
+    it('returns a promise', function() {
+      var messages = {
+          text: "I can haz promises instead of callbacks"
+      };
+
+      expect(slack.notify(messages).then).to.be.ok;
+      expect(slack.notify(messages).catch).to.be.ok;
+    });
+
+
+  });
+})
